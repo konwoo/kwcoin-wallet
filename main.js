70af404c4512a48a530db3f1024707dfc09fbc5e
konwoo <2konwoo@hanmail.net>
2018-12-05 16:41:37 +0900

    first commit, create index and main.js, in main.js electron.

---
+++ main.js
@@ -0,0 +1,28 @@
+const electron = require("electron"),
+  path = require("path"),
+  url = require("url");
+
+const { app, BrowserWindow } = electron;
+
+
+let mainWindow;
+
+
+
+function createWindow() {
+
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+    mainWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file",
+        slashes: true
+      })
+    );
+}
+
+app.on("ready", createWindow);
